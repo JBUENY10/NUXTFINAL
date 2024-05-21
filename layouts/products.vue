911772dd0cf52e74bc255bf4cba030aad681285d
JBUENY10 <juan.buendia.sanchez@gmail.com>
2024-05-21 12:45:51 +0200

    TRABAJO

---
+++ layouts/products.vue
@@ -0,0 +1,14 @@
+<template>
+<div class="ms-10 my-10 text-4xl font-thin text-purple-500">
+    <NuxtLink to="/">FakeStore</NuxtLink>
+</div>
+<slot />
+<div class="mx-10 my10 pt-5 flex fap-7 font-bold shadow-inner">
+    <button><NuxtLink to="/">home</NuxtLink></button>
+    <button><NuxtLink to="/">about</NuxtLink></button>
+    <button><NuxtLink to="/">products</NuxtLink></button>
+</div>
+
+</template>
+<script setup>
+</script>
